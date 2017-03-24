26376ec1cd83773a9c40d1e3d3289b46597dd759
Jony <liu.z.jonathan@gmail.com>
2017-03-24 12:44:10 -0700

    Converted to a function

--- read.py
+++ read.py
@@ -1,6 +1,11 @@
 import pickle
-with open('accounts.dat', 'rb') as f:
-    l = pickle.load(f)
+
+def read_accounts():
+    with open('accounts.dat', 'rb') as f:
+        l = pickle.load(f)
+    return l
     
-for i in l:
-    print("Name: %s\nEmail: %s\nPassword: %s\n\n" % (i[0], i[1], i[2]))
+if __name__ == '__main__':    
+    l = read_accounts()
+    for i in l:
+        print("Name: %s\nEmail: %s\nPassword: %s\n\n" % (i[0], i[1], i[2]))
