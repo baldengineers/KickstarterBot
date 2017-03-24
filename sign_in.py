82203a76245569afe7229b9a0c421ced8a0142fa
Jony <liu.z.jonathan@gmail.com>
2017-03-24 12:44:50 -0700

    Update sign_in.py

--- sign_in.py
+++ sign_in.py
@@ -1,1 +1,6 @@
-import selenium
+import read
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
+driver = webdriver.Chrome()
